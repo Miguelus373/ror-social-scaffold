9c32ae97ff64fb3f0fbed66bc5acd63ef1bcf1e5
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-11-02 16:06:15 -0400

    Update login tests

--- spec/requests/login_tests_spec.rb
+++ spec/requests/login_tests_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "LoginTests", type: :request do
-  scenario 'valid login input' do
+  scenario 'invalid login input' do
     visit new_user_session_path
     fill_in 'Email', with: 'hello@hello.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    visit root_path
-    expect(page).to have_content('stanley')
+    expect(page).to have_content('Invalid Email or password.')
   end
 end
